2caa516c331b3c4db887f197c706a4e625a23a9b
RainbowWolfer <RainbowWolfer@outlook.com>
2022-01-21 15:25:17 +0800

    Subscription Page Changed

--- Pages/SubscriptionPage.xaml.cs
+++ Pages/SubscriptionPage.xaml.cs
@@ -2,6 +2,7 @@
 using E621Downloader.Models.Posts;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -20,21 +21,134 @@ using Windows.UI.Xaml.Navigation;
 namespace E621Downloader.Pages {
 	public sealed partial class SubscriptionPage: Page {
 		private CancellationTokenSource cts = new CancellationTokenSource();
-		public SubscriptionPage() {
-			this.InitializeComponent();
+		private readonly List<FontIcon> icons = new List<FontIcon>();
+		private bool isSelecting = false;
+		private int previousIndex = -1;
 
-		}
+		private readonly ObservableCollection<FavoriteListViewItem> items = new ObservableCollection<FavoriteListViewItem>();
 
-		private async void Load() {
-			List<Post> posts = await Post.GetPostsByTagsAsync(cts.Token, true, 1, Local.FollowList);
-			foreach(Post item in posts ?? new List<Post>()) {
-				TestText.Text += item.file.url + "\n";
+		public bool IsSelecting {
+			get => isSelecting;
+			set {
+				isSelecting = value;
+				if(value) {
+					FavoritesListView.SelectionMode = ListViewSelectionMode.Multiple;
+					FavoritesListView.SelectedIndex = -1;
+					icons.ForEach(i => i.Width = 0);
+					DeleteButton.Visibility = Visibility.Visible;
+				} else {
+					FavoritesListView.SelectionMode = ListViewSelectionMode.Single;
+					if(previousIndex != -1) {
+						FavoritesListView.SelectedIndex = previousIndex;
+					}
+					icons.ForEach(i => i.Width = 40);
+					DeleteButton.Visibility = Visibility.Collapsed;
+					previousIndex = -1;
+				}
 			}
 		}
 
+		public SubscriptionPage() {
+			this.InitializeComponent();
+			for(int i = 0; i < FavoritesList.Table.Count; i++) {
+				FavoritesList item = FavoritesList.Table[i];
+				items.Add(new FavoriteListViewItem(i, item.Name, item.Items.Count));
+			}
+		}
+
+		//private async void Load() {
+		//	List<Post> posts = await Post.GetPostsByTagsAsync(cts.Token, true, 1, Local.FollowList);
+		//	foreach(Post item in posts ?? new List<Post>()) {
+		//		TestText.Text += item.file.url + "\n";
+		//	}
+		//}
+
 		protected override void OnNavigatedTo(NavigationEventArgs e) {
 			base.OnNavigatedTo(e);
-			Load();
+			//Load();
+		}
+
+		private void HamburgerButton_Tapped(object sender, TappedRoutedEventArgs e) {
+			MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+		}
+
+		private void FollowingButton_Tapped(object sender, TappedRoutedEventArgs e) {
+			e.Handled = true;
+			FavoritesListView.SelectedIndex = -1;
+			FollowingButton.IsChecked = true;
+		}
+
+		private void SelectionToggleButton_Tapped(object sender, TappedRoutedEventArgs e) {
+			e.Handled = true;
+			if(SelectionToggleButton.IsChecked.Value) {
+				previousIndex = FavoritesListView.SelectedIndex;
+				IsSelecting = true;
+			} else {
+				IsSelecting = false;
+			}
+		}
+
+		private void DeleteButton_Tapped(object sender, TappedRoutedEventArgs e) {
+
+		}
+
+		private void AddNewButton_Tapped(object sender, TappedRoutedEventArgs e) {
+
+		}
+
+		private void SplitViewDisplayModeSwitch_Toggled(object sender, RoutedEventArgs e) {
+			if(SplitViewDisplayModeSwitch.IsOn) {
+				MainSplitView.DisplayMode = SplitViewDisplayMode.CompactOverlay;
+			} else {
+				MainSplitView.DisplayMode = SplitViewDisplayMode.CompactInline;
+			}
+		}
+
+		private void RefreshButton_Tapped(object sender, TappedRoutedEventArgs e) {
+
+		}
+
+		private void FavoritesListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(IsSelecting) {
+
+			} else {
+				if(e.AddedItems.Count != 0) {
+					FollowingButton.IsChecked = false;
+				}
+			}
+		}
+
+		private void FontIcon_Loaded(object sender, RoutedEventArgs e) {
+			icons.Add(sender as FontIcon);
+
+		}
+
+		private void MainSplitView_PaneClosing(SplitView sender, SplitViewPaneClosingEventArgs args) {
+			IsSelecting = false;
+			SelectionToggleButton.IsChecked = false;
+		}
+
+		private void RefreshContentButton_Tapped(object sender, TappedRoutedEventArgs e) {
+
+		}
+
+		private void DeleteContentButton_Tapped(object sender, TappedRoutedEventArgs e) {
+
+		}
+
+		private void RenameButton_Tapped(object sender, TappedRoutedEventArgs e) {
+
+		}
+	}
+
+	public class FavoriteListViewItem {
+		public int Index { get; set; }
+		public string Title { get; set; }
+		public int Count { get; set; }
+		public FavoriteListViewItem(int index, string title, int count) {
+			Index = index;
+			Title = title;
+			Count = count;
 		}
 	}
 }
